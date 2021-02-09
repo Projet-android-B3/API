c4e0755eb97e78e4d055d9c6dd3a752f93370455
Arthur LE RAY <contact.arthurleray@gmail.com>
2021-02-09 08:30:36 +0100

    Added editTeam route

--- app/routes/team.routes.js
+++ app/routes/team.routes.js
@@ -6,4 +6,6 @@ module.exports = (app, db) => {
     const TeamController = require('../controllers/TeamController')(db);
 
     app.get('/getTeam/:user_id', TeamController.getTeam);
+
+    app.post('/getTeam/:user_id/edit', TeamController.editTeam);
 }
