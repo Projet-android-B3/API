49895788bbbe4da36fed7f764522c5fbf2ba47d3
Arthur LR <contact.arthurleray@gmail.com>
2021-03-25 19:20:51 +0100

    Added router.stack route

--- app/routes/base.routes.js
+++ app/routes/base.routes.js
@@ -6,4 +6,8 @@ module.exports = (app, db) => {
     app.get('/', function (req, res) {
         res.send("API works");
     });
+
+    app.get('/routes', function (req, res) {
+        res.send(app._router.stack);
+    });
 }
